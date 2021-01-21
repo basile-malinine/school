ccb6b348792ec53aac325ce3a93fcb0e7f35d785
basile <basile-m@yandex.ru>
2021-01-22 01:18:00 +0300

    Init YII framwork minimal

---
+++ config/web.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'id' => 'school-web',
+        'basePath' => realpath(__DIR__ . '/../'),
+        'aliases' => [
+            '@bower' => '@vendor/bower-asset',
+        ],
+    ];
